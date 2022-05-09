6f3686be6ef2e93c74e544a6c5583424862344ee
RikoStuff <chachaerickjo@gmail.com>
2022-05-09 21:50:57 +0300

    ft - add child class to inherit the animal class

--- src/main/java/chachaup/domain/Endangered.java
+++ src/main/java/chachaup/domain/Endangered.java
@@ -1,2 +1,14 @@
-package chachaup.domain;public class Endangered {
+package chachaup.domain;
+
+public class Endangered extends Animal{
+
+    private String health;
+    private int age;
+
+    public static final String HEALTHY = "healthy";
+
+    public Endangered(String animalName) {
+        super(animalName);
+
+    }
 }

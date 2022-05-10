631d0f3dcc7d954a757eef76d6c766700f6a9c0d
RikoStuff <chachaerickjo@gmail.com>
2022-05-10 10:07:20 +0300

    ft - add configurations for the frontend routing

--- src/main/java/chachaup/Main.java
+++ src/main/java/chachaup/Main.java
@@ -1,7 +1,33 @@
 package chachaup;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
-}
+    public static void main(String[] args) {
+            port(getHerokuAssignedPort());
+            staticFileLocation("/public");
+
+            // path to the home page
+        get("/",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index,hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+        }
+
+    }
